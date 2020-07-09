b8d88af5a2e6e562a61f030dbc8077f085390ea9
Property404 <martinez.dagan@gmail.com>
2020-07-08 22:01:07 -0500

    Add text UI

--- app.js
+++ app.js
@@ -1,46 +1,129 @@
+"use strict";
 import {Food, Direction, Snake} from './classes.js'
 const GRID_SIZE = 50;
+const MIN_DELAY = 30;
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext('2d');
+let enter_pressed;
+let snake;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function setHighScore(score)
+{
+	localStorage["snake-highscore"] = score;
+}
 
-const snake = new Snake(ctx, GRID_SIZE/2,GRID_SIZE/2,GRID_SIZE);
-snake.extend(1);
-snake.color="rgb(000,200,000)";
+function getHighScore(score)
+{
+	return localStorage["snake-highscore"]||0;
+}
 
-const food = new Food(ctx, GRID_SIZE);
-food.color="rgb(200,200,50)";
-food.draw();
+function displayScore(score)
+{
+	document.getElementById("score").textContent =
+		"Score: "+score;
+}
+
+function displayHighScore()
+{
+	document.getElementById("highscore").textContent =
+		"High Score: "+getHighScore();
+}
+
+function displayText(text)
+{
+	document.getElementById("text-overlay").innerHTML=text;
+}
+
+function clearAll()
+{
+	ctx.clearRect(0,0,canvas.width, canvas.height);
+	displayText("");
+}
+
+async function countDown()
+{
+	clearAll();
+	for(let i=3;i>=0;i--)
+	{
+		displayText(i?i:"Get Ready");
+		await sleep(300);
+	}
+	displayText("");
+}
+
+async function startGame()
+{
+		// Event loop
+		snake = new Snake(ctx, GRID_SIZE/2,GRID_SIZE/2,GRID_SIZE);
+		snake.extend(4);
+		snake.color="rgb(000,200,000)";
+		const food = new Food(ctx, GRID_SIZE);
+		food.color="rgb(200,200,50)";
+		let score = 0;
+		let delay = 50;
+		displayScore(score);
+		displayHighScore();
+		await countDown();
+		food.draw();
+		while(true)
+		{
+			snake.update();
+			if(snake.headMeetsSnake(snake))
+			{
+				// Game over!
+				let text = "Game Over";
+				if(score > getHighScore())
+				{
+					setHighScore(score);
+					text += "<br>New High Score!";
+				}
+				text+= "<br>Play again?";
+				displayText(text);
+				displayHighScore();
+				break;
+			}
+			if(snake.head.x<0)
+				snake.head.x = GRID_SIZE-1;
+			if(snake.head.x>=GRID_SIZE)
+				snake.head.x = 0;
+			if(snake.head.y<0)
+				snake.head.y = GRID_SIZE-1;
+			if(snake.head.y>=GRID_SIZE)
+				snake.head.y = 0;
+			if(snake.intersectsPoint(food))
+			{
+				score+=1;
+				displayScore(score);
+
+				// Get longer
+				snake.extend(1);
+
+				// Make new food
+				while(snake.intersectsPoint(food))
+					food.placeRandomly();
+				food.draw();
+
+				if(score%10===0 && delay>MIN_DELAY)
+					delay--;
+			}
+			await sleep(delay);
+		}
+}
 
 async function main()
 {
 	while(true)
 	{
-		snake.update();
-		if(snake.headMeetsSnake(snake))
-			throw("HIT");
-		if(snake.head.x<0)
-			snake.head.x = GRID_SIZE-1;
-		if(snake.head.x>=GRID_SIZE)
-			snake.head.x = 0;
-		if(snake.head.y<0)
-			snake.head.y = GRID_SIZE-1;
-		if(snake.head.y>=GRID_SIZE)
-			snake.head.y = 0;
-		if(snake.intersectsPoint(food))
+		await startGame()
+		enter_pressed = false;
+		while(!enter_pressed)
 		{
-			snake.extend(1);
-			while(snake.intersectsPoint(food))
-				food.placeRandomly();
-			food.draw();
-			console.log("GOBBLE");
+			await sleep(50);
 		}
-		await sleep(48);
-		
 	}
 }
 
@@ -66,6 +149,9 @@ window.onkeydown = function(e)
 		{
 			snake.direction = Direction.RIGHT;
 		}
+	}else if(key === "Enter")
+	{
+		enter_pressed = true;
 	}
 }
 main();
